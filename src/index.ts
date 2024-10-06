c11ebfa64b1519f56d2d26a888ea3a4fd58374cb
Sam <149597648+cheesesamwich@users.noreply.github.com>
2024-10-06 15:26:37 +0100

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import * as fs from "fs";
 import { uploader } from "./endpoints/uploader";
 import { auth } from "./endpoints/auth";
 import { cdn } from "./endpoints/cdn";
+import cors from "cors";
 
 export const app = express();
 
 const port = 9203;
+
+app.use(cors());
 
 uploader(app);
 
